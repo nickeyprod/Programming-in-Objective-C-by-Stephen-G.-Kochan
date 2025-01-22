aba1d35ba842844ad636fa01f133a097f6757b90
Nickolay <nickeyprod@gmail.com>
2025-01-22 15:07:04 +0300

    Chapter 2 - Program 3

---
+++ Chapter 2/Program 3/Program 3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Program 3
+//
+//  Created by Николай Ногин on 22.01.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
