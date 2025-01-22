934a774ed2985a982438b1a1c94669075fd99d9b
Nickolay <nickeyprod@gmail.com>
2025-01-22 15:07:51 +0300

    Chapter 2 - Program 3

--- Chapter 2/Program 3/Program 3/main.m
+++ Chapter 2/Program 3/Program 3/main.m
@@ -5,12 +5,20 @@
 //  Created by Николай Ногин on 22.01.2025.
 //
 
+// From book by Stephen G. Kochan - Programming in Objective-C. Sixth Edition.
+
+// Task: Write a program that subtracts the value 15 from 87 and displays the result.
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int first = 87;
+        int second = 15;
+        int result;
+        
+        result = first - second;
+        NSLog(@"The result of %i - %i is %i", first, second, result);
     }
     return 0;
 }
