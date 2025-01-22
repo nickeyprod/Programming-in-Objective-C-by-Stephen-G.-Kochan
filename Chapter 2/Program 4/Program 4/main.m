1429f9fd89c56d64b7b1b1e0d7df014e5a9393d1
Nickolay <nickeyprod@gmail.com>
2025-01-22 15:19:28 +0300

    Chapter 2 - Program 4

--- Chapter 2/Program 4/Program 4/main.m
+++ Chapter 2/Program 4/Program 4/main.m
@@ -5,12 +5,29 @@
 //  Created by Николай Ногин on 22.01.2025.
 //
 
+// From book by Stephen G. Kochan - Programming in Objective-C. Sixth Edition.
+
+// Task: Identify the syntactic errors in the following program. Then type in and run the corrected program to make sure you have identified all the mistakes:
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        INT sum; // not INT, but int
+        /* COMPUTE RESULT */
+        sum = 25 + 37 - 19 // not absent of semicolon, but semiolon (;)
+        / DISPLAY RESULTS / // not / comment /, but /* comment */
+        NSLog(@"The answer is %i" sum); // not NSLog(@"string" val), but (@"string", val)
+        /* =========================== */
+        
+      
+        // Right Program:
+        int sum;
+        /* COMPUTE RESULT */
+        sum = 25 + 37 - 19;
+        /* DISPLAY RESULTS */
+        NSLog(@"The answer is %i", sum);
     }
     return 0;
 }
