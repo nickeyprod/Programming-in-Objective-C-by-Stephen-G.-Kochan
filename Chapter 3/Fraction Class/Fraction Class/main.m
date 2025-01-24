01bdd0dba4e21f9af31eef5de06344c396a80532
Nickolay <nickeyprod@gmail.com>
2025-01-24 10:49:04 +0300

    Example of Fraction Class

---
+++ Chapter 3/Fraction Class/Fraction Class/main.m
@@ -0,0 +1,73 @@
+//
+//  main.m
+//  Fraction Class
+//
+//  Created by Николай Ногин on 24.01.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+// Example of Class implementation for working with fractions
+
+//---- @interface section ----
+
+@interface Fraction : NSObject
+
+// Instance methods
+- (void) print;
+// Setters
+- (void) setNumerator: (int) n;
+- (void) setDenominator: (int) d;
+// Getters
+- (int)  numerator;
+- (int)  denominator;
+
+@end
+
+@implementation Fraction {
+    int numerator;
+    int denominator;
+}
+
+- (void) print {
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+- (void) setNumerator: (int) n {
+    numerator = n;
+}
+
+- (void) setDenominator: (int) d {
+    denominator = d;
+}
+
+- (int) numerator {
+    return numerator;
+}
+
+- (int) denominator {
+    return denominator;
+}
+
+@end
+
+//---- Program section ----
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Fraction *myFraction = [[Fraction alloc] init];
+        
+        // Set fraction to 1/3
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator: 3];
+        
+        // Display the fraction using instance's print method
+        NSLog(@"The value of myFraction is:");
+        [myFraction print];
+        
+        // Display the fraction using our two new methods
+        NSLog(@"The value of myFraction is: %i/%i",
+              [myFraction numerator], [myFraction denominator]);
+    }
+    return 0;
+}
